1975e76f7e69029a54ed041636a4f9274915ccde
João Marcos Azevedo Cruz <136996299+JoaoM9081@users.noreply.github.com>
2024-01-30 12:26:26 -0300

    first commit

---
+++ Código/main.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+# validando entrada int
+def ler_int(msg):
+    while True:
+        try:
+            t = int(input(msg).strip())
+        except ValueError:
+            print('\033[1;31mEntrada inválida, tente digitar um número inteiro válido\033[m')
+        else:
+            return t
+
+t = ler_int('\033[1mDigite o tempo em (segundos) para o timer: \033[m')
+
+# Quando t = 0, retornará False
+while t:
+    segundos, minutos = divmod(t, 60) # convertendo minutos e segundos
+    timer = f'{segundos:02d}:{minutos:02d}'
+    print(f'\033[1;34mTimer:\033[m \033[1;36m{timer}\033[m', end="\r")
+    sleep(1) # pausa de 1s
+    t -= 1
+    
+print('\n\033[1;31mTEMPO ACABOU !!!\033[m')
---
+++ README.md
@@ -0,0 +1,14 @@
+# Temporizador em Python
+
+Este projeto é um temporizador simples implementado em Python.
+
+## Descrição
+
+O programa solicita ao usuário que insira um tempo em segundos. Em seguida, ele inicia um temporizador que conta regressivamente até zero.
+
+## Como usar
+
+1. Execute o script Python no seu terminal.
+2. Quando solicitado, insira o tempo desejado em segundos.
+3. O temporizador começará a contar regressivamente.
+4. Quando o tempo acabar, você verá a mensagem "TEMPO ACABOU !!!".
